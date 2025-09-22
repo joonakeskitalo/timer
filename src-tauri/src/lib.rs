877ba12eb1f323aeef2f380a984ae9a1c7ebe3ae
Joona Keskitalo <joona.keskitalo@gmail.com>
2025-09-22 20:19:05 +0300

    feat: init tauri app

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,23 @@
+use uuid::Uuid;
+use tauri::{ LogicalSize,Size };
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+async fn create_window(app: tauri::AppHandle) {
+  let id = Uuid::new_v4();
+  let webview_window = tauri::WebviewWindowBuilder::new(&app, id, tauri::WebviewUrl::App("index.html".into()))
+    .title("Timer")
+    .build()
+    .unwrap();
+
+    webview_window.set_size(Size::Logical(LogicalSize { width: 480.0, height: 255.0})).unwrap()
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![create_window])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
